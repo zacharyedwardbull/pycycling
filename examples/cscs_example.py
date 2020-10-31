069ea9d694fcacff7e7d34874d8cc08d57565a3d
Zachary Bull <zacharyedwardbull@gmail.com>
2020-10-31 15:38:17 +0000

    Add basic implementation of CSCS

---
+++ examples/cscs_example.py
@@ -0,0 +1,27 @@
+import asyncio
+from bleak import BleakClient
+
+from pycycling.cycling_speed_cadence_service import CyclingSpeedCadenceService
+
+
+async def run(address):
+    async with BleakClient(address) as client:
+        def my_page_handler(data):
+            print(data)
+
+        await client.is_connected()
+        trainer = CyclingSpeedCadenceService(client)
+        trainer.set_csc_measurement_handler(my_page_handler)
+        await trainer.enable_csc_measurement_notifications()
+        await asyncio.sleep(30.0)
+        await trainer.disable_csc_measurement_notifications()
+
+
+if __name__ == "__main__":
+    import os
+
+    os.environ["PYTHONASYNCIODEBUG"] = str(1)
+    address = "EAAA3D1F-6760-4D77-961E-8DDAC1CC9AED"  # <--- Change to your device's address here if you are using macOS
+    loop = asyncio.get_event_loop()
+    # loop.set_debug(True)
+    loop.run_until_complete(run(address))
---
+++ pycycling/cycling_speed_cadence_service.py
@@ -0,0 +1,50 @@
+from collections import namedtuple
+
+csc_measurement_tx_id = '00002a5b-0000-1000-8000-00805f9b34fb'
+
+CSCMeasurement = namedtuple('CSCMeasurement',
+                            ['cumulative_wheel_revs', 'last_wheel_event_time', 'cumulative_crank_revs',
+                             'last_crank_event_time'])
+
+
+class CyclingSpeedCadenceService:
+    def __init__(self, client):
+        self._client = client
+        self._csc_measurement_callback = None
+
+    async def enable_csc_measurement_notifications(self):
+        await self._client.start_notify(csc_measurement_tx_id, self._csc_measurement_notification_handler)
+
+    async def disable_csc_measurement_notifications(self):
+        await self._client.stop_notify(csc_measurement_tx_id)
+
+    def set_csc_measurement_handler(self, callback):
+        self._csc_measurement_callback = callback
+
+    def _csc_measurement_notification_handler(self, sender, data):
+        flags = data[0]
+
+        wheel_rev_included_flag = 1
+        crank_rev_included_flag = 2
+
+        cumulative_wheel_revs = None
+        last_wheel_event_time = None
+        cumulative_crank_revs = None
+        last_crank_event_time = None
+
+        byte_offset = 1
+
+        if flags & wheel_rev_included_flag:
+            cumulative_wheel_revs = int.from_bytes(data[0 + byte_offset:4 + byte_offset], 'little')
+            last_wheel_event_time = int.from_bytes(data[4 + byte_offset:6 + byte_offset], 'little')
+            byte_offset += 6
+
+        if flags & crank_rev_included_flag:
+            cumulative_crank_revs = int.from_bytes(data[0 + byte_offset:2 + byte_offset], 'little')
+            last_crank_event_time = int.from_bytes(data[2 + byte_offset:4 + byte_offset], 'little')
+
+        if self._csc_measurement_callback is not None:
+            self._csc_measurement_callback(CSCMeasurement(cumulative_wheel_revs=cumulative_wheel_revs,
+                                                          last_wheel_event_time=last_wheel_event_time,
+                                                          cumulative_crank_revs=cumulative_crank_revs,
+                                                          last_crank_event_time=last_crank_event_time))
