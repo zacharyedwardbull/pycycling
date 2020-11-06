b460528da5767991dc303bad5c108852f9095657
Zachary Bull <zacharyedwardbull@gmail.com>
2020-11-06 14:40:26 +0000

    Add cycling speed and cadence feature

--- pycycling/cycling_speed_cadence_service.py
+++ pycycling/cycling_speed_cadence_service.py
@@ -1,10 +1,13 @@
 from collections import namedtuple
 
 csc_measurement_tx_id = '00002a5b-0000-1000-8000-00805f9b34fb'
+csc_feature_tx_id = '00002a5c-0000-1000-8000-00805f9b34fb'
 
 CSCMeasurement = namedtuple('CSCMeasurement',
                             ['cumulative_wheel_revs', 'last_wheel_event_time', 'cumulative_crank_revs',
                              'last_crank_event_time'])
+
+CSCFeature = namedtuple('CSCFeature', ['wheel_rev_supported', 'crank_rev_supported', 'multiple_locations_supported'])
 
 
 class CyclingSpeedCadenceService:
@@ -20,6 +23,17 @@ class CyclingSpeedCadenceService:
 
     def set_csc_measurement_handler(self, callback):
         self._csc_measurement_callback = callback
+
+    async def get_csc_feature(self):
+        measurement = await self._client.read_gatt_char(csc_feature_tx_id)
+        value = int.from_bytes(measurement, byteorder='little')
+
+        wheel_rev_supported = bool(value & 0b1)
+        crank_rev_supported = bool(value & 0b10)
+        multiple_locations_supported = bool(value & 0b100)
+
+        return CSCFeature(wheel_rev_supported=wheel_rev_supported, crank_rev_supported=crank_rev_supported,
+                          multiple_locations_supported=multiple_locations_supported)
 
     def _csc_measurement_notification_handler(self, sender, data):
         flags = data[0]
