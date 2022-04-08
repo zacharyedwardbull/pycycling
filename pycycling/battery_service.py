883152ae8ebca437552065d29e94eb8111687bfc
Zachary Bull <zacharyedwardbull@gmail.com>
2022-04-08 13:40:13 +0100

    Add basic unit tests for byte parsing operations

--- pycycling/battery_service.py
+++ pycycling/battery_service.py
@@ -1,10 +1,14 @@
 battery_level_characteristic_id = '00002a19-0000-1000-8000-00805f9b34fb'
 
 
+def _parse_battery_level(measurement):
+    return int.from_bytes(measurement, byteorder='little')
+
+
 class BatteryService:
     def __init__(self, client):
         self._client = client
 
     async def get_battery_level(self):
         measurement = await self._client.read_gatt_char(battery_level_characteristic_id)
-        return int.from_bytes(measurement, byteorder='little')
+        return _parse_battery_level(measurement)
--- pycycling/heart_rate_service.py
+++ pycycling/heart_rate_service.py
@@ -5,6 +5,44 @@ heart_rate_measurement_characteristic_id = '00002a37-0000-1000-8000-00805f9b34fb
 HeartRateMeasurement = namedtuple('HeartRateMeasurement', ['sensor_contact', 'bpm', 'rr_interval', 'energy_expended'])
 
 
+def _parse_hr_measurement(data):
+    flags = data[0]
+
+    is_uint16_measurement_mask = 0x01
+    is_contact_detected_mask = 0x06
+    is_energy_expended_present_mask = 0x08
+    is_rr_interval_present_mask = 0x10
+
+    sensor_contact = None
+    bpm = None
+    rr_interval = []
+    energy_expended = None
+
+    measurement_byte_offset = 1
+    sensor_contact = bool(flags & is_contact_detected_mask)
+
+    if flags & is_uint16_measurement_mask:
+        bpm = int.from_bytes(data[measurement_byte_offset:measurement_byte_offset + 2], 'little')
+        measurement_byte_offset += 2
+    else:
+        bpm = data[measurement_byte_offset]
+        measurement_byte_offset += 1
+
+    if flags & is_energy_expended_present_mask:
+        energy_expended = int.from_bytes(data[measurement_byte_offset:measurement_byte_offset + 2], 'little')
+        measurement_byte_offset += 2
+
+    if flags & is_rr_interval_present_mask:
+        while len(data[measurement_byte_offset:]) >= 2:
+            rr_interval.append(int.from_bytes(data[measurement_byte_offset:measurement_byte_offset + 2], 'little'))
+            measurement_byte_offset += 2
+
+    return HeartRateMeasurement(sensor_contact=sensor_contact,
+                                bpm=bpm,
+                                rr_interval=rr_interval,
+                                energy_expended=energy_expended)
+
+
 class HeartRateService:
     def __init__(self, client):
         self._client = client
@@ -21,42 +59,5 @@ class HeartRateService:
         self._hr_measurement_callback = callback
 
     def _hr_measurement_notification_handler(self, sender, data):
-
-        flags = data[0]
-
-        is_uint16_measurement_mask = 0x01
-        is_contact_detected_mask = 0x06
-        is_energy_expended_present_mask = 0x08
-        is_rr_interval_present_mask = 0x10
-
-        sensor_contact = None
-        bpm = None
-        rr_interval = []
-        energy_expended = None
-
-        measurement_byte_offset = 1
-
-        sensor_contact = bool(flags & is_contact_detected_mask)
-
-        if flags & is_uint16_measurement_mask:
-            bpm = int.from_bytes(data[measurement_byte_offset:measurement_byte_offset + 2], 'little')
-            measurement_byte_offset += 2
-        else:
-            bpm = data[measurement_byte_offset]
-            measurement_byte_offset += 1
-
-        if flags & is_energy_expended_present_mask:
-            energy_expended = int.from_bytes(data[measurement_byte_offset:measurement_byte_offset + 2], 'little')
-            measurement_byte_offset += 2
-
-        if flags & is_rr_interval_present_mask:
-            while len(data[measurement_byte_offset:]) >= 2:
-                rr_interval.append(int.from_bytes(data[measurement_byte_offset:measurement_byte_offset + 2], 'little'))
-                measurement_byte_offset += 2
-
         if self._hr_measurement_callback is not None:
-            self._hr_measurement_callback(
-                HeartRateMeasurement(sensor_contact=sensor_contact,
-                                     bpm=bpm,
-                                     rr_interval=rr_interval,
-                                     energy_expended=energy_expended))
+            self._hr_measurement_callback(_parse_hr_measurement(data))
