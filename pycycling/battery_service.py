edd8680fa0dd58f52a813a59e235bd528f227726
Zachary Bull <zacharyedwardbull@gmail.com>
2022-04-09 18:22:14 +0100

    Add documentation to battery_service module

--- pycycling/battery_service.py
+++ pycycling/battery_service.py
@@ -1,3 +1,13 @@
+"""
+A module for interacting with Bluetooth devices which support the Battery Service.
+
+Example
+=======
+Print the current battery level to the console:
+
+.. literalinclude:: ../examples/battery_example.py
+"""
+
 battery_level_characteristic_id = '00002a19-0000-1000-8000-00805f9b34fb'
 
 
@@ -6,9 +16,22 @@ def _parse_battery_level(measurement):
 
 
 class BatteryService:
+    """
+    A wrapper around a :obj:`bleak.backends.client.BaseBleakClient` object adding Battery Service specific utility
+    methods.
+
+    :param client: A valid :obj:`bleak.backends.client.BaseBleakClient` object
+    """
+
     def __init__(self, client):
         self._client = client
 
     async def get_battery_level(self):
+        """
+        Returns current battery level of the Bluetooth device.
+
+        :return: An :obj:`int` representing the current battery level percentage of the Bluetooth device. A value of
+            `100` indicates a fully charged battery while `0` a fully discharged battery.
+        """
         measurement = await self._client.read_gatt_char(battery_level_characteristic_id)
         return _parse_battery_level(measurement)
