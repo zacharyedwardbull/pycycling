de5a2d5d53faa60224234aed274c61330abf89cd
Zachary Bull <zacharyedwardbull@gmail.com>
2023-11-23 19:04:44 +0000

    Bump version

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r", encoding="utf8") as fh:
 
 setuptools.setup(
     name="pycycling",
-    version="0.3.2",
+    version="0.3.3",
     author="Zachary Bull",
     author_email="zacharyedwardbull@gmail.com",
     description=
